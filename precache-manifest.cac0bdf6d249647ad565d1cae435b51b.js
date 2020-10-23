d39e36e7a7cae1066060599d3078eda30ff992c7
lumosfinance <lumosfinances@protonmail.com>
2020-10-23 13:29:27 +0300

    abrakadabra

---
+++ precache-manifest.cac0bdf6d249647ad565d1cae435b51b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f78c031f250c9279129b1d9c42cdb34",
+    "url": "https://lumos.finance/index.html"
+  },
+  {
+    "revision": "52c5ccfa082b59ce31c8",
+    "url": "https://lumos.finance/static/css/main.cb33edc2.chunk.css"
+  },
+  {
+    "revision": "70f2156f9917e9c64f78",
+    "url": "https://lumos.finance/static/js/0.e42fa017.chunk.js"
+  },
+  {
+    "revision": "befecc5e13eb02e4d9ad",
+    "url": "https://lumos.finance/static/js/10.639a5da1.chunk.js"
+  },
+  {
+    "revision": "ebc6f349370c7c641615",
+    "url": "https://lumos.finance/static/js/11.e7014088.chunk.js"
+  },
+  {
+    "revision": "ab9832cb1c7fdf6e1062",
+    "url": "https://lumos.finance/static/js/3.959c676f.chunk.js"
+  },
+  {
+    "revision": "15723851b11509ef1b9a8dbd2a0ef589",
+    "url": "https://lumos.finance/static/js/3.959c676f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5110bac17722ebcb244d",
+    "url": "https://lumos.finance/static/js/4.7aec9497.chunk.js"
+  },
+  {
+    "revision": "ca3bec7f160d23dc551f",
+    "url": "https://lumos.finance/static/js/5.8a382a30.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "https://lumos.finance/static/js/5.8a382a30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daaa742f9a6921e75823",
+    "url": "https://lumos.finance/static/js/6.e0bdc13e.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "https://lumos.finance/static/js/6.e0bdc13e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b866bc7ee614b55540e3",
+    "url": "https://lumos.finance/static/js/7.ddf9f2f9.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "https://lumos.finance/static/js/7.ddf9f2f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92b662c948b3e3826a3",
+    "url": "https://lumos.finance/static/js/8.6a3a3533.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "https://lumos.finance/static/js/8.6a3a3533.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c0de999ef075fe5bc85",
+    "url": "https://lumos.finance/static/js/9.276b4905.chunk.js"
+  },
+  {
+    "revision": "52c5ccfa082b59ce31c8",
+    "url": "https://lumos.finance/static/js/main.507b2ee7.chunk.js"
+  },
+  {
+    "revision": "d7e430f4ab71e30147c1",
+    "url": "https://lumos.finance/static/js/runtime-main.47a191c2.js"
+  },
+  {
+    "revision": "75aab891878b744c7bf7ec2a97106d87",
+    "url": "https://lumos.finance/static/media/bodoni72.75aab891.woff"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "https://lumos.finance/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "d1f736120a29462726fb25a8e88cffba",
+    "url": "https://lumos.finance/static/media/wallet-connect.d1f73612.svg"
+  }
+]);
